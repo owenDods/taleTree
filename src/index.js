401099f3aef18ed57c50f6ebb7934b7e02c2041a
Owen Dods <owendods@gmail.com>
2021-03-27 11:43:20 +0000

    Add in support for image loading and generate html via webpack plugin

--- src/index.js
+++ src/index.js
@@ -8,12 +8,18 @@ import './styles/index.scss';
 // render(<App />, document.getElementById('root'));
 
 import join from 'lodash/fp/join';
+import Icon from './img/triangle.svg';
 
 function component() {
 
 	const element = document.createElement('div');
 
 	element.innerHTML = join(' ', [ 'Hello', 'webpack' ]);
+
+	const myIcon = new Image();
+	myIcon.src = Icon;
+
+	element.appendChild(myIcon);
 
 	return element;
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,28 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './src/index.js',
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist')
-	},
 	module: {
 		rules: [
 			{
 				test: /\.(scss|css)$/i,
 				use: [ 'style-loader', 'css-loader', 'postcss-loader', 'sass-loader' ]
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource'
 			}
 		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'TaleTree',
+			hash: true
+		})
+	],
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist')
 	}
 };
