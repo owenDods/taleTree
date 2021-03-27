a6410aa95385542c827271127f99f6215c53197c
Owen Dods <owendods@gmail.com>
2021-03-27 11:06:13 +0000

    Webpack boilerplate

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import { render } from 'react-dom';
+// import React from 'react';
+// import { render } from 'react-dom';
 
 import './styles/index.scss';
 
-import App from './components/app/App';
+// import App from './components/app/App';
 
-render(<App />, document.getElementById('root'));
+// render(<App />, document.getElementById('root'));
+
+import join from 'lodash/fp/join';
+
+function component() {
+
+	const element = document.createElement('div');
+
+	element.innerHTML = join(' ', [ 'Hello', 'webpack' ]);
+
+	return element;
+
+}
+
+document.body.appendChild(component());
