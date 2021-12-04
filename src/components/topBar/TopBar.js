73d5f1bb2cd27a80ebde6fea59984a9c01caea19
Owen Dods <owendods@gmail.com>
2021-12-04 14:37:38 +0000

    First pass at TopBar logic + styles

--- src/components/topBar/TopBar.js
+++ src/components/topBar/TopBar.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import getRouteLabel from './utils/getRouteLabel';
+
 export const className = 'topBar';
 
-const TopBar = () => (<div className={className} />);
+const TopBar = () => (
+
+	<div className={className}>
+
+		<h1>{getRouteLabel()}</h1>
+
+	</div>
+
+);
 
 export default TopBar;
