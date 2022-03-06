a856b3de4155532128d79cacea8fe00dd05de18c
Owen Dods <owendods@gmail.com>
2022-03-06 18:07:41 +0000

    First pass at styling basic TaleMap elements

--- src/components/taleMap/TaleMap.js
+++ src/components/taleMap/TaleMap.js
@@ -2,6 +2,16 @@ import React from 'react';
 
 export const className = 'taleMap';
 
-const TaleMap = () => (<div className={className} />);
+const TaleMap = () => (
+
+	<div className={className}>
+
+		<div className={`${className}__end`} />
+
+		<div className={`${className}__start`} />
+
+	</div>
+
+);
 
 export default TaleMap;
