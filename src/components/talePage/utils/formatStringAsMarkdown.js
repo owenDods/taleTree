a6c58e6b47b5d842081741684eba46a7bd96e650
Owen Dods <owendods@gmail.com>
2022-03-12 20:10:02 +0000

    Add in support for formatting page text with bold tags

--- src/components/talePage/utils/formatStringAsMarkdown.js
+++ src/components/talePage/utils/formatStringAsMarkdown.js
@@ -1,7 +1,54 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import map from 'lodash/fp/map';
+import forEach from 'lodash/fp/forEach';
 
-export default (text = '') => (
-	map.convert({ cap: false })((textParagraph, i) => (<p key={i}>{textParagraph}</p>), text.split('//'))
-);
+const formatStringWithBoldMarkdown = string => {
+
+	const boldTagsArray = string.split('/b/');
+
+	if (boldTagsArray.length === 1) {
+
+		return string;
+
+	}
+
+	const boldMarkdownArray = [];
+
+	forEach.convert({ cap: false })((boldStringSection, i) => {
+
+		const key = `${string}-${boldStringSection}-${i}`;
+
+		if (i % 2) {
+
+			boldMarkdownArray.push(<b key={key}>{boldStringSection}</b>);
+
+		} else {
+
+			boldMarkdownArray.push(<Fragment key={key}>{boldStringSection}</Fragment>);
+
+		}
+
+	})(boldTagsArray);
+
+	return boldMarkdownArray;
+
+};
+
+export default (string = '') => {
+
+	const paragraphArray = string.split('//');
+
+	return map.convert({ cap: false })((paragraphString, i) => {
+
+		const formattedString = formatStringWithBoldMarkdown(paragraphString);
+
+		return (
+			<p key={`${string}-${paragraphString}-${i}`}>
+				{formattedString}
+			</p>
+		);
+
+	})(paragraphArray);
+
+};
